461630217ee92d8b14aaf68e845ab2a1dfe37197
Marvin Truong <khatruong06@gmail.com>
2026-03-07 23:30:32 +1000

    change format in cpp file

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main()
 {
     string name;
-    cin >> name
-                   cout
-               << "Hello " << name << "?";
+    cin >> name;
+    cout << "Hello " << name << "!";
     return 0;
 }
